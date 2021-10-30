a6b0ce188a7dbbdb188586d3396cdb71ccd15a91
TejasViswa <45400093+TejasViswa@users.noreply.github.com>
2021-10-29 20:05:16 -0700

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -1,16 +1,38 @@
 import numpy as np
 
 class myclassifier:
-    def __init__(self) -> None:
-        pass
+    def __init__(self,dataset = np.zeros((100,785))):
+        if not isinstance(x, pd.DataFrame):
+            dataset = np.zeros((100,785))
+            print("Dataset is not a Pandas DataFrame")
+        self.train_data, self.train_label = train.drop(labels = ["label"],axis = 1).to_numpy(), train["label"]
+        # Assuming L is 1, which makes W as (m features x 1) vector and w as scalar
+        self.W = np.zeros((self.train_data.shape,1))
+        self.w = np.zeros(1)
 
-    def sample_selection(self,training_sample,):
+    def sample_selection(self,training_sample):
         """
         training_sample = row of dataset that is input to be determined whther or nt it will be added to the dataset
         Return:
             Output of this function should be the MyClassifier object that keeps the updated data set
         """
-        pass
+        
+#         if not isinstance(x, pd.DataFrame):
+#             Sample = np.zeros((1,785))
+#             print("Sample is not a Pandas DataFrame")
+
+        # Separate Features and Labels
+        sample_X,sample_Y = training_sample.drop(labels = ["label"],axis = 1).to_numpy(), training_sample["label"]
+        
+        # Some logic to check if sample data is useful
+        logic = True
+        
+        # Updating training dataset after verifying some logic
+        if(logic)
+            self.train_data.append(sample_X)
+            self.train_label.append(sample_Y)
+        
+        return self
 
     def train(self,train_data,train_label):
         """
@@ -20,14 +42,31 @@ class myclassifier:
         Return:
             Output of this function should be the MyClassifier object which has the information of weights, bias, number of classes, number of features
         """
-        pass
+        
+        # calculate hinge loss
+        N = train_data.shape[0]
+        distances = 1 - f(train_label) * np.matrix.dot(self.W.getT(),train_label) + self.w
+        distances[distances < 0] = 0  # equivalent to max(0, distance)
+        hinge_loss = reg_strength * (np.sum(distances) / N)
+
+        # calculate cost
+        cost = 1 / 2 * np.dot(self.W, self.W) + hinge_loss
+        
+        return self
 
     def f(self,input):
         """
-        Returns:
+        input = this is the input vector to f(.), which corresponds to the function g(y) = W'*y + w which is an L dimensional vector
+        Return:
             Output is an estimated class
         """
-        np.dot(input,self.W) + self.w
-
+        # Let us first define a variable for the function g(y) = W'*y + w
+        g = np.matrix.dot(self.W.getT(),input) + self.w
+        
+        # We return estimated class as +1 or -1 when g is positive and non-positive respectively
+        s = 1 if all([x > 1 for x in g]) else -1
+        
+        return s
+        
     def test(self):
         pass
