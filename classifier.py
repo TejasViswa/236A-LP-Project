6d2edfd643323c9820be84a20fecedff7ee3d906
TejasViswa <45400093+TejasViswa@users.noreply.github.com>
2021-10-29 21:01:04 -0700

    Update classifier.py

--- classifier.py
+++ classifier.py
@@ -1,27 +1,44 @@
 import numpy as np
+import pandas as pd
 
-class myclassifier:
-    def __init__(self):
-        self.W = np.zeros()
-        self.w = np.zeros()
-        self.train_data = np.zeros()
-        self.train_label = np.zeros()
+class MyClassifier_25:
+    def __init__(self,dataset = np.zeros((100,785))):
+        
+        # By default, a 100 x 785 zero matrix is taken as dataset
+        if not isinstance(dataset, pd.DataFrame):
+            dataset = np.zeros((100,785))
+            print("Dataset is not a Pandas DataFrame")
+        
+        # Separating training data and training label
+        self.train_data, self.train_label = dataset.drop(labels = ["label"],axis = 1).to_numpy(), dataset["label"]
+        
+        # W is matrix of m rows and L columns and w is a L - dimensional vector
+        # Assuming L is 1, which makes W as (m features x 1) vector and w as scalar:
+        self.W = np.zeros((self.train_data.shape[1],1))
+        self.w = np.zeros(1)
 
-    def sample_selection(self,training_sample,):
+
+    def sample_selection(self,training_sample):
         """
         training_sample = row of dataset that is input to be determined whther or nt it will be added to the dataset
         Return:
             Output of this function should be the MyClassifier object that keeps the updated data set
         """
         
+#         if not isinstance(x, pd.DataFrame):
+#             Sample = np.zeros((1,785))
+#             print("Sample is not a Pandas DataFrame")
+
         # Separate Features and Labels
-        sample_X,sample_Y = training_sample[:-2], training_sample[-1]
+        sample_X,sample_Y = training_sample[:-1], training_sample[-1]
         
         # Some logic to check if sample data is useful
+        logic = True
         
-        # Updating training dataset
-        self.train_data.append(sample_X)
-        self.train_label.append(sample_Y)
+        # Updating training dataset after verifying some logic
+        if(logic):
+            self.train_data = np.vstack([self.train_data,sample_X])
+            self.train_label = np.vstack([self.train_label,sample_X])
         
         return self
 
@@ -54,8 +71,10 @@ class myclassifier:
         # Let us first define a variable for the function g(y) = W'*y + w
         g = np.matrix.dot(self.W.getT(),input) + self.w
         
-        # We return estimated class as +1 or -1 when g is non-negative and negative respectively
-        return 1 if g >= 0 else -1
+        # We return estimated class as +1 or -1 when g is positive and non-positive respectively
+        s = 1 if all([x > 1 for x in g]) else -1
+        
+        return s
         
     def test(self):
         pass
