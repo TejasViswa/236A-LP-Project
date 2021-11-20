ff3515ca6b1cb4e12c1cb91325efebc98e44e09e
anwxwna <anwchatto@gmail.com>
2021-11-20 12:31:35 -0800

    main

--- main.py
+++ main.py
@@ -10,5 +10,6 @@ test_dataset = pd.read_csv('/home/anw/ucla_coursework/236A/project/mnist/mnist_t
 if __name__ == "__main__":
     classfier =  MyClassifier_25(dataset,1,7)
     results, performance = classfier.test(test_dataset)
+    print("trained")
     correct = classfier.assess_classifier_performance(performance)
     print("Correctly classified ",correct)
